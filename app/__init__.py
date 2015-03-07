929649ae6f8779d72af4a892d6814adbeff7d73a
adriel <AdrielVelazquez@gmail.com>
2015-03-07 15:03:11 -0500

    Upload file succesfully

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 from app import routes
 
 from routes import classification
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,29 @@
-from flask import Blueprint, render_template, request
+import os
+from collections import defaultdict
 
-from app.views import *
+from flask import Blueprint, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + "/static/imgs/temp"
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+flower_dictionary = defaultdict(dict)
 
 classification = Blueprint('classification', __name__, url_prefix='/classification',
                     template_folder='templates', static_folder='static')
 
-@classification.route("/")
+
+@classification.route("/", methods=["GET", "POST"])
+def default():
+    return redirect(url_for("classification.main_application"))
+
+@classification.route("/upload", methods=["POST", "GET"])
 def main_application():
-    return render_template("index.html")
+    if request.method == "POST":
+        file = request.files['file']
+        if file:
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(UPLOAD_FOLDER, filename)
+            file.save(file_path)
+            return render_template("index.html", process="primary", file_path=file_path)
+    return render_template("index.html", process="upload")
