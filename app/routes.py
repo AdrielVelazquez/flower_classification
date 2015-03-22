1de1abe2e8ed57ea66a55406b70295d4235670da
adriel <AdrielVelazquez@gmail.com>
2015-03-22 18:05:16 -0400

    More Changes to csv saving and stem colors

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,13 @@
 import os
 from collections import defaultdict
+import csv
 
 from flask import Blueprint, render_template, request, redirect, url_for, jsonify
 from werkzeug.utils import secure_filename
 from PIL import Image
 
 UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + "/static/imgs/temp"
+CSV_FOLDER = os.path.dirname(os.path.abspath(__file__)) + "/static/csv/master.csv"
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 image_size = (304, 228)
 
@@ -21,6 +23,9 @@ def default():
 
 @classification.route("/upload", methods=["POST", "GET"])
 def main_application():
+    process = request.args.get("process")
+    if process == "submit":
+        submit()
     if request.method == "POST":
         file = request.files['file']
         if file:
@@ -54,7 +59,6 @@ def save_count():
     filename = request.args.get("filename").split("/")[-1]
     count = int(request.args.get("count"))
     flower_dictionary[filename]["count"] = count
-    print flower_dictionary
     return jsonify({"RGB": 0})
 
 @classification.route("/picker", methods=["GET"])
@@ -68,3 +72,19 @@ def petal_count():
     filename = request.args.get("filename")
     process = request.args.get("process")
     return render_template("index.html", process=process, file_path=filename)
+
+@classification.route("/stem_color", methods=["GET"])
+def stem_color():
+    filename = request.args.get("filename")
+    process = request.args.get("process")
+    return render_template("index.html", process=process, file_path=filename)
+
+
+def submit():
+    filename = request.args.get("filename").split("/")[-1]
+    flower_dict = flower_dictionary[filename]
+    line = [filename]
+    keys = ['count','stem_color_RGB','primary_RGB','secondary_RGB']
+    line.extend([flower_dict.get(key) for key in keys])
+    writer = csv.writer(open(CSV_FOLDER, 'a'))
+    writer.writerow(line)
